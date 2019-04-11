c925eca72e2b3d15cff94b3c8df34d95b0fe35a7
Zheng Wei Yap <zhengwei.yap@sparkbeyond.com>
2019-04-11 11:26:37 +0800

    removed for loops for all methods

--- normalization.py
+++ normalization.py
@@ -1,31 +1,19 @@
-def normalization(df, method='full_standardization',offset = 0,numeric_cols=None,window=0,key_col=None):
+def normalization(df, method='full_standardization', offset=0, numeric_cols=None, window=0, key_col=None):
     
     import pandas as pd
     import numpy as np
     
     if method == 'full_standardization':
         fulldf = df.copy(deep=True)
-        fulldf[numeric_cols] = (fulldf[numeric_cols]-np.mean(fulldf[numeric_cols]))/np.std(fulldf[numeric_cols])
+        fulldf[numeric_cols] = (fulldf[numeric_cols] - np.mean(fulldf[numeric_cols])) / np.std(fulldf[numeric_cols])
         return fulldf
             
     if method == 'long_term_average':
-        fulldf = None 
-        for key in df[key_col].unique():
-            key_df=df.loc[df[key_col] == key,numeric_cols]
-            mean_df = key_df.rolling(window=window,min_periods=1).mean().shift(offset)
-            norm_df = key_df/mean_df
-            df1 = pd.concat([df[df[key_col] == key].drop(columns=numeric_cols),norm_df],axis=1,sort=False)
-            fulldf = df1 if fulldf is None else fulldf.append(df1,sort=False,ignore_index=True)
+        fulldf = df.copy(deep=True) 
+        fulldf[numeric_cols] = fulldf.groupby(key_col)[numeric_cols].apply(lambda x: x/x.rolling(window=window, min_periods=1).mean().shift(offset))
         return fulldf
 
-    
     if method == 'standardization':
-        fulldf = None 
-        for key in df[key_col].unique():
-            key_df=df.loc[df[key_col] == key,numeric_cols]
-            mean_df = key_df.rolling(window=window,min_periods=1).mean().shift(offset)
-            std_df =  key_df[numeric_cols].rolling(window=window,min_periods=1).std().shift(offset)
-            norm_df = (key_df-mean_df)/std_df
-            df1 = pd.concat([df[df[key_col] == key].drop(columns=numeric_cols),norm_df],axis=1,sort=False)
-            fulldf = df1 if fulldf is None else fulldf.append(df1,sort=False,ignore_index=True)
+        fulldf = df.copy(deep=True) 
+        fulldf[numeric_cols] = fulldf.groupby(key_col)[numeric_cols].apply(lambda x: (x-x.rolling(window=window, min_periods=1).mean().shift(offset))/x.rolling(window=window, min_periods=1).std().shift(offset))
         return fulldf
